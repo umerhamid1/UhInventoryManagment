7780545ab1c0c61483b7a2da3600d8933f37c036
umer <smomerhamid@gmail.com>
2019-11-29 02:20:07 -0800

    basic is completed

---
+++ InventoryManagment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InventoryManagment
+//
+//  Created by umer hamid on 11/28/19.
+//  Copyright © 2019 umer hamid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
